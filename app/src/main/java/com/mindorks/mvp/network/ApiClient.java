fc120255bb0608f6d73b67b0e9e4567a02ac3e06
nyamuk <asamsulfat13@gmail.com>
2018-04-29 05:59:38 +0700

    add apiclient and api helper

--- app/src/main/java/com/mindorks/mvp/network/ApiClient.java
+++ app/src/main/java/com/mindorks/mvp/network/ApiClient.java
@@ -1,8 +1,33 @@
 package com.mindorks.mvp.network;
 
-/**
- * Created by nyamuk on 4/28/18.
- */
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.converter.scalars.ScalarsConverterFactory;
 
 public class ApiClient {
-}
+
+    private static OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
+
+    private static Retrofit.Builder builder = new Retrofit.Builder()
+            .baseUrl("http://filkom.ub.ac.id/module/api/conf/")
+            .client(httpClient.build())
+            .addConverterFactory(ScalarsConverterFactory.create())
+            .addConverterFactory(GsonConverterFactory.create()
+            );
+
+    private static Retrofit retrofit = builder.build();
+
+    public static <S> S createService(Class<S> serviceClass) {
+
+        HttpLoggingInterceptor loggingInterceptor = new HttpLoggingInterceptor();
+        loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+
+        httpClient.addInterceptor(loggingInterceptor);
+
+        builder.client(httpClient.build());
+        retrofit = builder.build();
+
+        return retrofit.create(serviceClass);
+    }}
--- app/src/main/java/com/mindorks/mvp/network/ApiHelper.java
+++ app/src/main/java/com/mindorks/mvp/network/ApiHelper.java
@@ -1,8 +1,15 @@
 package com.mindorks.mvp.network;
 
+import retrofit2.Call;
+import retrofit2.http.GET;
+
 /**
  * Created by nyamuk on 4/28/18.
  */
 
-public class ApiHelper {
+public interface ApiHelper {
+
+    //get data dosen
+    @GET("get_civitas/dosen/")
+    Call<String> getDosenData();
 }
